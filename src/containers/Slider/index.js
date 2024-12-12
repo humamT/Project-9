33dc2e10c9338e662eed3c5d7c26cce3a574a006
humamT <humam.01@hotmail.com>
2024-12-12 12:49:52 +0100

    removed unnecessary comments

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,63 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useData } from "../../contexts/DataContext";
-// import { getMonth } from "../../helpers/Date";
-
-// import "./style.scss";
-
-// const Slider = () => {
-//   const { data } = useData();
-//   const [index, setIndex] = useState(0);
-//   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-//     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-//   );
-//   const nextCard = () => {
-//     setTimeout(
-//       () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-//       5000
-//     );
-//   };
-//   useEffect(() => {
-//     nextCard();
-//   });
-//   return (
-//     <div className="SlideCardList">
-//       {byDateDesc?.map((event, idx) => (
-//         <>
-//           <div
-//             key={event.title}
-//             className={`SlideCard SlideCard--${
-//               index === idx ? "display" : "hide"
-//             }`}
-//           >
-//             <img src={event.cover} alt="forum" />
-//             <div className="SlideCard__descriptionContainer">
-//               <div className="SlideCard__description">
-//                 <h3>{event.title}</h3>
-//                 <p>{event.description}</p>
-//                 <div>{getMonth(new Date(event.date))}</div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="SlideCard__paginationContainer">
-//             <div className="SlideCard__pagination">
-//               {byDateDesc.map((_, radioIdx) => (
-//                 <input
-//                   key={`${event.id}`}
-//                   type="radio"
-//                   name="radio-button"
-//                   checked={idx === radioIdx}
-//                 />
-//               ))}
-//             </div>
-//           </div>
-//         </>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Slider;
-
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
@@ -68,10 +8,21 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
 
+  //   const byDateDesc = data?.focus.sort((evtA, evtB) =>
   const byDateDesc = data?.focus.slice().sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
 
+
+  //   const nextCard = () => {
+  //     setTimeout(
+  //       () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+  //       5000
+  //     );
+  //   };
+  //   useEffect(() => {
+  //     nextCard();
+  //   });
   useEffect(() => {
     const timer = setTimeout(() => {
       setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
@@ -91,13 +42,20 @@ const Slider = () => {
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
-                <div>{event.date ? getMonth(new Date(event.date)) : "Date unavailable"}</div>
+                {/* <div>{getMonth(new Date(event.date))}</div>   Assumes event.date always exists. */}
+                <div>{event.date ? getMonth(new Date(event.date)) : "Date unavailable"}</div>  { /* Adds a fallback for missing dates with "Date unavailable" */}
               </div>
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((dot) => (
+                //     <input
+                //     key={`${event.id}`}
+                //     type="radio"
+                //     name="radio-button"
+                //     checked={idx === radioIdx}
+                //     />
                 <input
                   key={`radio-${event.id}`}
                   type="radio"
